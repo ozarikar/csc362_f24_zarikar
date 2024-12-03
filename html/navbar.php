c98e560d4b502fad10686d1ad089cf5d241de1e4
ozarikar <omkar.zarikar@centre.edu>
2024-12-03 04:31:40 +0000

    added a file for nav bar

--- html/navbar.php
+++ html/navbar.php
@@ -2,8 +2,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <style>
-        /* Simple styling for the navigation bar */
+<style>
+        /* styling for the navigation bar */
         nav {
             background-color: #333;
             overflow: hidden;
@@ -22,19 +22,21 @@
             text-decoration: none;
             display: block;
         }
-        nav ul li a:hover {
-            background-color: #111;
-        }
+        
     </style>
 </head>
 <body>
+    <!-- Navigation Bar -->
     <nav>
         <ul>
-            <li><a href="home.php">Home</a></li>
-            <?php
+        <li><a href="home.php">Home</a></li>    
+        <?php
             if (isset($_SESSION['user_role'])) {
                 if ($_SESSION['user_role'] == 1) { // Employee
                     echo '<li><a href="inventory.php">Inventory</a></li>';
+                    echo '<li><a href="partners.php">Partners</a></li>';
+                    echo '<li><a href="transactions.php">Transactions</a></li>';
+
                 }
                 if ($_SESSION['user_role'] == 0) { // Customer
                     echo '<li><a href="cart.php">Cart <img src="cart_icon.png" alt="Cart Icon" style="width:20px; height:20px; filter: invert(1);"></a></li>';
@@ -48,7 +50,9 @@
                 echo '<li><a href="catalog.php?category=' . htmlspecialchars($category['id']) . '">' . htmlspecialchars($category['name']) . '</a></li>';
             }
             ?>
+            <li><a href="logout.php"><img src="log_out_icon.png" alt="Log Out Icon" style="width:20px; height:20px;filter: invert(1);"></a></li>
         </ul>
     </nav>
+
 </body>
 </html>
