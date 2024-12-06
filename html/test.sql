1e50800d3c68d9dfab86e1213ac028ae364634c2
ozarikar <omkar.zarikar@centre.edu>
2024-12-06 09:00:00 +0000

    added more attributes to table

--- html/test.sql
+++ html/test.sql
@@ -2,12 +2,15 @@ SELECT
     p.product_id,
     p.product_name,
     pb.product_brand_name,
-    GROUP_CONCAT(pl.product_length ORDER BY pl.product_length ASC) AS product_lengths
+    GROUP_CONCAT(DISTINCT pl.product_length ORDER BY pl.product_length ASC) AS product_lengths,
+    GROUP_CONCAT(DISTINCT ps.product_size ORDER BY ps.product_size ASC) AS product_sizes
 FROM 
     products p
 LEFT JOIN 
     product_brands pb ON p.product_brand_id = pb.product_brand_id
 LEFT JOIN 
     products_length pl ON p.product_id = pl.product_id
+LEFT JOIN 
+    products_size ps ON p.product_id = ps.product_id
 GROUP BY 
     p.product_id, p.product_name, pb.product_brand_name;
